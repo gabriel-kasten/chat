8f0e16999e8373c6975b85247016aae436dd02c9
Gabriel Deglmann Kasten <gabrieldegkasten@gmail.com>
2023-02-05 00:05:15 -0300

    🔥 new bugs in front-end!!!!

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const messageArea = document.querySelector('#messages');
+const input_messsage_field = document.querySelector('#input_messsage_field')
+const send_message_button = document.querySelector('#send_message_button');
 
-const messages = document.querySelectorAll('.message');
-
-const input_field = document.querySelector('#input_field');
-const send_message = document.querySelector('#send_message');
-
-send_message.addEventListener("click", sendMessage);
-input_field.addEventListener("keypress", (e) => {
+send_message_button.addEventListener("click", sendMessage);
+input_messsage_field.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     sendMessage();
   }
 })
 
 function sendMessage() {
-  var content = input_field.value;
-  input_field.value = "";
+  var content = input_messsage_field.value;
+  content.value = "";
 
   var time = new Date();
-
   var formatedTime = `Hoje - ${time.getHours()}:${time.getMinutes()} `;
-  var message = `<div class="message you"><div class="send">${formatedTime}</div><div class="body">${content}</div></div>`
 
-  messages[messages.length - 1].insertAdjacentHTML('beforeend', message);
+  let message = `<div class="message you">
+                  <div class="top">${formatedTime}</div>
+                  <div class="body">${content}</div>
+                </div>`
+  const messages = document.querySelector(".messages");
 
-  scrollBottom();
+  messages.lastElementChild.insertAdjacentHTML('afterend', message);
+  messages.lastElementChild.scrollIntoView();
+
+  console.log(messages);
 }
